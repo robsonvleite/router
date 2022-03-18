d24ab445d21ea5839ccd097791638dce920bd29f
Beto Campoy <beto.campoy@gmail.com>
2022-03-18 15:48:08 -0300

    Renaming default key_id var name

--- src/Router.php
+++ src/Router.php
@@ -81,16 +81,16 @@ class Router extends Dispatch
     public function resource(string $route, $handler, string $name = null): void
     {
         $singularized_route = singularize($route);
-        $key_id = "{" . str_replace("/", "", $singularized_route) ."_id}";
+//        $key_id = "{" . str_replace("/", "", $singularized_route) ."_id}";
 
         $this->addRoute("GET", $route, $handler.":list", ($name ? "{$name}.list" : null));
         $this->addRoute("GET", "{$route}/home", $handler.":list", ($name ? "{$name}.home" : null));
         $this->addRoute("GET", "{$route}/home/{search}/{page}", $handler.":list", ($name ? "{$name}.searchGet" : null));
         $this->addRoute("GET", $singularized_route, $handler.":create", ($name ? "{$name}.create" : null));
-        $this->addRoute("GET", singularize($route)."/{$key_id}", $handler.":edit", ($name ? "{$name}.edit" : null));
+        $this->addRoute("GET", singularize($route)."/{:id}", $handler.":edit", ($name ? "{$name}.edit" : null));
         $this->addRoute("POST", "{$route}/search", $handler.":search", ($name ? "{$name}.searchPost" : null));
         $this->addRoute("POST", $singularized_route, $handler.":store", ($name ? "{$name}.store" : null));
-        $this->addRoute("POST", singularize($route)."/{$key_id}", $handler.":update", ($name ? "{$name}.update" : null));
-        $this->addRoute("DELETE", singularize($route)."/{$key_id}", $handler.":delete", ($name ? "{$name}.delete" : null));
+        $this->addRoute("POST", singularize($route)."/{:id}", $handler.":update", ($name ? "{$name}.update" : null));
+        $this->addRoute("DELETE", singularize($route)."/{:id}", $handler.":delete", ($name ? "{$name}.delete" : null));
     }
 }
